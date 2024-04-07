eff79422a0e00e168656c57480158b4bf77c0724
Rahul Yadav <rahul.yadav6131766@gmail.com>
2024-04-07 10:34:14 +0530

    Setting up PostgreSQL database

--- core/settings.py
+++ core/settings.py
@@ -85,11 +85,16 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+# Using PostgreSQL
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
