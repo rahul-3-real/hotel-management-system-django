e4c00b507667c2058184280878f4e2daee14797e
Rahul Yadav <rahul.yadav6131766@gmail.com>
2024-04-14 14:15:17 +0530

    Fixed url error

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,36 +8,35 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
-    # Does not work
-    """
-    path(
-        "password-reset/",
-        auth_views.PasswordResetView.as_view(
-            template_name="accounts/password-reset.html",
-            success_url="accounts:password-reset-complete",
-        ),
-        name="password-reset",
-    ),
-    path(
-        "password-reset-done/",
-        auth_views.PasswordResetDoneView.as_view(
-            template_name="accounts/password-reset-done.html",
-        ),
-        name="password-reset-done",
-    ),
-    path(
-        "password-reset-confirm/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name="accounts/password-reset-confirm.html",
-        ),
-        name="password-reset-confirm",
-    ),
-    path(
-        "password-reset-complete/",
-        auth_views.PasswordResetCompleteView.as_view(
-            template_name="accounts/password-reset-complete.html",
-        ),
-        name="password-reset-complete",
-    ),
-    """,
 ]
+
+# Does not work
+
+# path(
+#     "password-reset/",
+#     auth_views.PasswordResetView.as_view(
+#         template_name="accounts/password-reset.html",
+#     ),
+#     name="password-reset",
+# ),
+# path(
+#     "password-reset-done/",
+#     auth_views.PasswordResetDoneView.as_view(
+#         template_name="accounts/password-reset-done.html",
+#     ),
+#     name="password-reset-done",
+# ),
+# path(
+#     "password-reset-confirm/<uidb64>/<token>/",
+#     auth_views.PasswordResetConfirmView.as_view(
+#         template_name="accounts/password-reset-confirm.html",
+#     ),
+#     name="password-reset-confirm",
+# ),
+# path(
+#     "password-reset-complete/",
+#     auth_views.PasswordResetCompleteView.as_view(
+#         template_name="accounts/password-reset-complete.html",
+#     ),
+#     name="password-reset-complete",
+# ),
